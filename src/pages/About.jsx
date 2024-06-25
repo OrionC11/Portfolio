c78acde72ea124f2625d5cef2a7a75289c8b4f89
Orion Cannon <orion.m.cannon@gmail.com>
2024-06-25 10:43:56 -0700

    added more to the about me text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,10 @@ const About = () => {
       <img src="/portrait.jpg" className="portrait" />
       <p className="aboutText">
         I am a web developer with a passion for creating and designing websites.
-        I have experience with HTML, CSS, JavaScript, and React. I just
-        completed the Full Stack Flex Bootcamp from ASU!
+        I have experience with HTML, CSS, JavaScript, REACT, MERN Stack, and
+        SQL. I just completed the Full Stack Flex Bootcamp from ASU! Fun fact
+        about me: I love going out with my girlfriend Rebecca and our dog Cooper
+        on hikes and adventures!
       </p>
     </div>
   );
